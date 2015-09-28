52e5f5e0627b011ad3b9ea0e80acc91c359b0049
Matthias Reis <matthias.reis@xing.com>
2015-09-28 11:35:17 +0200

    final fixture nodes

--- test/config.js
+++ test/config.js
@@ -1,5 +1,8 @@
 x1.paths({
   x1: '/base/client/js/x1',
   node1: ['/base/test/fixtures/node11.js', '/base/test/fixtures/node12.js'],
-  node2: ['/base/test/fixtures/node2.js']
+  node21: ['/base/test/fixtures/node21.js'],
+  node22: ['/base/test/fixtures/node22.js'],
+  node31: ['/base/test/fixtures/node31.js'],
+  node32: ['/base/test/fixtures/node32.js']
 });
